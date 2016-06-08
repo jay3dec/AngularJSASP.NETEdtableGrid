2c2de48af8445de02db4820728b5080502129b6f
Jay Raj <jay.r@thinkpalm.lan>
2016-06-08 17:05:01 +0530

    first

---
+++ EditableGrid/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EditableGrid.Models;
+
+namespace EditableGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Default Method 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Action Method to Get Employee Details
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetEmployees()
+        {
+            List<Employee> empList = new List<Employee>();
+
+            Employee emp = new Employee { FirstName = "James", LastName = "Bond", Country = "Germany" };
+            empList.Add(emp);
+
+            emp = new Employee { FirstName = "Roy", LastName = "Agasthyan", Country = "United States" };
+            empList.Add(emp);
+
+            return Json(new { employees = empList });
+        }
+    }
+}
